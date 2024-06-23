ad0b5c2ced58e8b49d582cba46162eb0f0fa13dd
iwakusumah <haiyahhhhh@gmail.com>
2024-06-23 19:26:26 +0700

    update sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -12,7 +12,7 @@
         </div>
     </div>
     <div class="sb-sidenav-footer">
-        <div class="small">Logged in as:</div>
+        <div class="small">Login Sebagai:</div>
         {{ Auth::user()->nama }}
     </div>
 </nav>
