791f130e5c8b2d84bfb1e2b76c779cf6012f23e8
iwakusumah <haiyahhhhh@gmail.com>
2024-06-24 08:19:30 +0700

    update detail

--- resources/views/employee/show.blade.php
+++ resources/views/employee/show.blade.php
@@ -10,7 +10,7 @@
         </div>
         <div class="card-body">
             <p class="card-text"><strong>Nama:</strong> {{ $employee->nama }}</p>
-            <p class="card-text"><strong>Jabatan:</strong> {{ $employee->jabatan }}</p>
+            <p class="card-text"><strong>Posisi:</strong> {{ $employee->jabatan }}</p>
             <p class="card-text"><strong>Status:</strong> {{ $employee->status }}</p>
             <p class="card-text"><strong>Gaji:</strong> Rp. {{ number_format($employee->gaji, 2, ',', '.')}}</p>
             <p class="card-text"><strong>Tanggal Masuk:</strong> {{ $employee->tanggal_masuk }}</p>
